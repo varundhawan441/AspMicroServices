06bbbbaaf726476adc3a35fc0cd347c0ac0938e1
varun.dhawan <varun.dhawan@kochartech.com>
2022-06-21 10:50:08 +0530

    Create Data and Repositories for catalog

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<String>("DatabaseSetting:DatabaseConnection"));
+            var database = client.GetDatabase(configuration.GetValue<String>("DatabaseSetting:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<String>("DatabaseSetting:DatabaseCollectionName"));
+            // Run when application startup check any data exist in product collection or not
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
